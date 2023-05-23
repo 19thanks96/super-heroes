f3a9f2b1829af27649b590b1acf3bb6e0d8f9766
hellgamer <19thanks96@gmail.com>
2023-05-23 17:24:23 +0300

    add button for create new hero

--- frontend/src/components/SuperheroList.tsx
+++ frontend/src/components/SuperheroList.tsx
@@ -12,7 +12,9 @@ export type SuperHero = {
 export const SuperheroList: React.FC<SuperheroListProps> = (props) => {
     if(props?.superheroes?.length === 0) {
         return(
+            <>
             <h1>You have no heroes created yet</h1>
+            <button>Add new hero</button></>
         )
     }
     return (
--- frontend/src/test/SuperheroList.test.tsx
+++ frontend/src/test/SuperheroList.test.tsx
@@ -7,3 +7,9 @@ test('renders  "you have no heroes created yet" when no heroes pass', () => {
     const superheroElement = screen.getByText(/you have no heroes created yet/i);
     expect(superheroElement).toBeInTheDocument();
   });
+
+test('renders `add new hero` button when no heroes pass', () => {
+  render(<SuperheroList superheroes={[]} /> );
+  const superheroElement = screen.getByText(/add new hero/i);
+  expect(superheroElement).toBeInTheDocument();
+});
