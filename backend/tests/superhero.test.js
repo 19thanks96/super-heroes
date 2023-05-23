66995f265aeb7db8e34c99249471708c96d0dbc5
hellgamer <19thanks96@gmail.com>
2023-05-23 15:13:13 +0300

    add test for list superheroes

---
+++ backend/tests/superhero.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest')
+const app = require('../app')
+const superman = {
+  nickname: 'Superman',
+  real_name: 'Clark Kent',
+  origin_description: `he was born Kal-El on the planet Krypton, before being rocketed to
+  Earth as an infant by his scientist father Jor-El, moments before Krypton's destruction…
+  superpowers: solar energy absorption and healing factor, solar flare and heat vision,
+  solar invulnerability, flight…`,
+  catch_phrase:
+    "Look, up in the sky, it's a bird, it's a plane, it's Superman!",
+}
+const superheroes = [superman]
+describe('Superhero', () => {
+  test('get /superheroes should return list of superheroes', async () => {
+    const res = await request(app).get('/superheroes')
+    expect(res.statusCode).toEqual(200)
+    expect(res.body).toBe(superheroes)
+  })
+})
