cd2972d573c2cbafce647abc16a49273005a6ea9
hellgamer <19thanks96@gmail.com>
2023-05-23 16:56:38 +0300

    add frontend test for superheroes

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders superheroes header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const superheroElement = screen.getByText(/superheroes/i);
+  expect(superheroElement).toBeInTheDocument();
 });
+
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <h1>Superheroes</h1>
+      
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,7 +15,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
