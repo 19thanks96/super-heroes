90de8d2f105f23ae60aaee17ba2ecebb23e5575b
hellgamer <19thanks96@gmail.com>
2023-05-23 14:19:27 +0300

    add backend

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
