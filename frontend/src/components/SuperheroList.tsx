d76ac9b8aebeaa0c6b4fbb5aa0f423ba6d5e6584
hellgamer <19thanks96@gmail.com>
2023-05-23 20:16:32 +0300

    add test for button and click button

--- frontend/src/components/SuperheroList.tsx
+++ frontend/src/components/SuperheroList.tsx
@@ -1,29 +1,24 @@
+import { Link } from "react-router-dom"
+
 export type SuperheroListProps = {
-    superheroes : Array<SuperHero>;
+    superheroes: Array<SuperHero>
 }
 
 export type SuperHero = {
-    nickname : string;
-    real_name: string;
-    origin_description: string;
-    catch_phrase: string;
+    nickname: string
+    real_name: string
+    origin_description: string
+    catch_phrase: string
 }
 
 export const SuperheroList: React.FC<SuperheroListProps> = (props) => {
-    function handleClick () {
-        
-    }
-    if(props?.superheroes?.length === 0) {
-        return(
+    if (props?.superheroes?.length === 0) {
+        return (
             <>
-            <h1>You have no heroes created yet</h1>
-            <button onClick={handleClick}>Add new hero</button></>
+                <h1>You have no heroes created yet</h1>
+                <Link to="/createhero">Add new hero</Link>
+            </>
         )
     }
-    return (
-        <>
-
-        </>
-    )
+    return <></>
 }
-
--- frontend/src/test/SuperheroList.test.tsx
+++ frontend/src/test/SuperheroList.test.tsx
@@ -1,20 +1,41 @@
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import { SuperheroList } from '../components/SuperheroList';
+import React from 'react'
+import { render, screen, fireEvent } from '@testing-library/react'
+import { SuperheroList } from '../components/SuperheroList'
+import { MemoryRouter, Router, Route, BrowserRouter } from 'react-router-dom'
+import userEvent from '@testing-library/user-event'
 
+const renderWithRouter = (
+    ui: React.ReactElement<any, string | React.JSXElementConstructor<any>>,
+    { route = '/' } = {}
+) => {
+    window.history.pushState({}, 'Test page', route)
+
+    return {
+        user: userEvent.setup(),
+        ...render(ui, { wrapper: BrowserRouter }),
+    }
+}
 
 test('renders  "you have no heroes created yet" when no heroes pass', () => {
-    render(<SuperheroList superheroes={[]} /> );
-    const superheroElement = screen.getByText(/you have no heroes created yet/i);
-    expect(superheroElement).toBeInTheDocument();
-  });
+    render(<SuperheroList superheroes={[]} />, { wrapper: BrowserRouter })
+    const superheroElement = screen.getByText(/you have no heroes created yet/i)
+    expect(superheroElement).toBeInTheDocument()
+})
+
+test('renders  "add new hero" button', () => {
+  render(<SuperheroList superheroes={[]} />, { wrapper: BrowserRouter })
+  const superheroElement = screen.getByRole('link', {
+    name: /add new hero/i,
+})
+  expect(superheroElement).toBeInTheDocument()
+})
 
 test('renders `add new hero` button when no heroes pass', async () => {
-  render(<SuperheroList superheroes={[]} /> );
-  const superheroElement = screen.getByRole('button', {
-    name: /add new hero/i
-  })
-  fireEvent.click(superheroElement)
-  await screen.findByText(/create your hero/i)
-  expect(superheroElement).toBeInTheDocument();
-});
+    const user = userEvent.setup()
+    renderWithRouter(<SuperheroList superheroes={[]} />)
+    const superheroElement = screen.getByRole('link', {
+        name: /add new hero/i,
+    })
+    await user.click(superheroElement)
+    expect(window.location.pathname).toEqual('/createhero')
+})
